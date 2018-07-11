31635924fe13469909aa906b54e2ba0ed2ea5de5
aniket2098 <aniket.mp@gmail.com>
2018-07-11 18:10:53 +0530

    TextView issue fixed.

--- app/src/main/java/com/example/admin/credenz18/Register.java
+++ app/src/main/java/com/example/admin/credenz18/Register.java
@@ -11,18 +11,20 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class Register extends Activity {
+public class Register extends AppCompatActivity{
     ArrayList<Event> events;
-    RegisterAdapter adapter;
     TextView textView;
     int total;
     View view;
@@ -31,8 +33,83 @@ public class Register extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         Toolbar toolbar = findViewById(R.id.toolbar);
+         class RegisterAdapter extends
+                RecyclerView.Adapter<RegisterAdapter.ViewHolder> {
+
+             class ViewHolder extends RecyclerView.ViewHolder {
+
+                public TextView event;
+                public CheckBox box;
+                public TextView price;
+
+                public ViewHolder(View itemView) {
+                    super(itemView);
+                    event = (TextView) itemView.findViewById(R.id.event);
+                    price=itemView.findViewById(R.id.price);
+                    box = (CheckBox) itemView.findViewById(R.id.checkbox);
+                }
+            }
+            @Override
+            public RegisterAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                Context context = parent.getContext();
+                LayoutInflater inflater = LayoutInflater.from(context);
+                View contactView = inflater.inflate(R.layout.recycler_item, parent, false);
+                ViewHolder viewHolder = new ViewHolder(contactView);
+                return viewHolder;
+            }
+
+            private List<Event> event;
+            public List<Event> receipt=new ArrayList<>();
+
+
+            public RegisterAdapter(List<Event> events) {
+                event = events;
+            }
+
+            @Override
+            public int getItemCount() {
+                return event.size();
+            }
+            @Override
+            public void onBindViewHolder(final RegisterAdapter.ViewHolder viewHolder, final int position) {
+                final Event register = event.get(position);
+                TextView textView = viewHolder.event;
+                textView.setText(register.getName());
+                textView = viewHolder.price;
+                textView.setText(String.valueOf(register.getPrice()));
+                final CheckBox checkBox=viewHolder.box;
+                checkBox.setChecked(event.get(position).getCheck());
+                viewHolder.box.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        int pos = viewHolder.getAdapterPosition();
+                        if(checkBox.isChecked()){
+                            receipt.add(event.get(pos));
+                            event.get(position).modify(position,true);
+                            total+=event.get(position).getPrice();
+                        }
+                        else{
+                            event.get(position).modify(position,false);
+                            String temp=event.get(pos).getName();
+                            for(int i=0;i<receipt.size();i++)
+                            {
+                                if(temp.equals(receipt.get(i).getName()))
+                                {
+                                    receipt.remove(i);
+                                    total-=event.get(pos).getPrice();
+                                }
+
+                            }
+                        }
+                        ((TextView)findViewById(R.id.total)).setText(Integer.toString(total));
+                    }
+                });
+            }
+        }
+        RegisterAdapter adapter;
+
         textView = findViewById(R.id.total);
-//        setSupportActionBar(toolbar);
+        setSupportActionBar(toolbar);
         view = this.findViewById(android.R.id.content);
         final RecyclerView rvContacts = findViewById(R.id.rv);
         events = Event.createContactsList();
@@ -49,19 +126,5 @@ public class Register extends Activity {
                 startActivity(intent);
             }
         });
-
-//        rvContacts.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Toast.makeText(Register.this, "Successful!", Toast.LENGTH_LONG).show();
-//            }
-//        });
-
-    }
-
-    public void modify() {
-
-        ((TextView)view.findViewById(R.id.total)).setText("Hello world");
-
     }
 }
