1b8d78109a4d559840461d65aedc98c142c16cbb
aniket2098 <aniket.mp@gmail.com>
2018-07-19 20:04:30 +0530

    Local database extended to store the entire data.

--- app/src/main/java/com/example/admin/credenz18/RegAdapter.java
+++ app/src/main/java/com/example/admin/credenz18/RegAdapter.java
@@ -4,11 +4,9 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import androidx.recyclerview.widget.RecyclerView;
 
